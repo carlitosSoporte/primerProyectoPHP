668b915cdd0905f9af39d3aac269ac35e4a6ff96
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2020-10-15 09:38:10 -0500

    se hace hola mundo en php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    $nombreUsuario = "Carlos Diaz";
+    $edad =  40;
+    $estatura = 1.80;
+    $nombreUsuario = "Juan Perez";
+
+    define("PI",3.141516);
+
+    echo($nombreUsuario." sea usted bienvenido"." ".PI);
+
+?>
