21f7b76afe428689c2dc51ff912f5b4bb5daf235
carlitosSoporte <59172826+carlitosSoporte@users.noreply.github.com>
2020-10-20 11:19:25 -0500

    avances php

---
+++ formularioProductos.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Ejercicios PHP</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Ejercicios PHP</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <div class="container mt-5">
+                <div class="row justify-content-center">
+                    <div class="col-6">
+                        <form action="formularioProductos.php" method="POST">
+                            <div class="row">
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="Producto" name="nombreProducto">
+                                </div>
+                                <div class="col">
+                                    <input type="number" class="form-control" placeholder="Precio" name="precioProducto">
+                                </div>
+                            </div>
+                            <div class="row justify-content-center mt-3">
+                                <div class="col-4">
+                                    <button type="submit" class="btn btn-primary mb-2" name="botonEnviar">Enviar datos</button>
+                                </div>
+                            </div> 
+                        </form>
+                        <?php
+
+                            if(isset($_POST["botonEnviar"])){
+                                $nombre = $_POST["nombreProducto"];
+                                $precio = $_POST["precioProducto"];
+                                $precio = $precio + 5000;
+                                echo("el producto es ".$nombre." y su precio es de $".$precio);
+                            }
+
+                        ?>
+                    </div>           
+                </div>
+            </div>
+        </main>
+        <footer>
+
+        </footer>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    </body>
+</html>
--- tienda.php
+++ tienda.php
@@ -1,3 +1,36 @@
 <?php
+$producto1 = "doritos";
+$producto2 = "today";
+$producto3 ="yuca";
+$producto4 = "papas";
+$producto5 = "aceite";
+$producto6 = "frutino";
+$producto7 = "sal";
+$producto8 = "carne res";
+$producto9 = "tomate";
+$producto10 = "bananos";
 
+$precio1 = 8000;
+$precio2 = 13000;
+$precio3 = 5000;
+$precio4 = 6000;
+$precio5 = 10000;
+$precio6 = 3000;
+$precio7 = 1000;
+$precio8 = 30000;
+$precio9 = 2000;
+$precio10 = 4000;
+
+$totalCompra = $precio1 + $precio2 + $precio3 + $precio4 + $precio5 + $precio6 +$precio7+ $precio8 + $precio9 + $precio10;
+
+
+echo("El total de la compra es ".$totalCompra);
+echo("<br>");
+
+if($totalCompra <= 80000){
+    echo("Gracias por su compra");
+}
+else{
+    echo("Debe validar su compra, eres pobre.");
+}
 ?>
